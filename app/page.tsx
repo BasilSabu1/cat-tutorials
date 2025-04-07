cfab6dd43198969f802650f0faa3208f797964b8
BasilSabu <141615788+BasilSabu1@users.noreply.github.com>
2025-04-07 18:29:04 +0530

    coverted the file to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,11 @@
-import Header from "@/app/components/header/Header";
-import BannerPage from "@/app/components/banner/Banner";
-import Review from "@/app/components/review/Review";
-import Coursecomparison from "@/app/components/coursecomparision/Course";
-import Faq from "@/app/components/faq/Faq";
-import Comparison from "@/app/components/comparison/Comparison";
-import Faculty from "@/app/components/faculty/Faculty";
-import Benefits from "@/app/components/benefits/Benefits";
-import Footer from "@/app/components/footer/Footer";
+import Homepage from "@/app/components/home/Home";
 
 
 
 export default function Home() {
   return (
   <div className="">
-   <Header/>
-   <BannerPage/>
-   <Benefits/>
-   <Faculty/>
-   <Comparison/>
-   <Review/>
-   <Coursecomparison/>
-   <Faq/>
-   <Footer/>
+  <Homepage/>
   </div>
   );
 }
