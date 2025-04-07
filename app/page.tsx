0452081ff8556f37fd08f1ae04822ca003aad993
BasilSabu <141615788+BasilSabu1@users.noreply.github.com>
2025-04-07 18:34:37 +0530

    added ssr in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-'use client';
+'use client'; // Add this at the top
 
-import Homepage from "@/app/components/home/Home";
+import dynamic from 'next/dynamic';
 
-
+// Dynamically import Homepage with SSR disabled
+const Homepage = dynamic(
+  () => import('@/app/components/home/Home'),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
-  <div className="">
-  <Homepage/>
-  </div>
+    <div className="">
+      <Homepage />
+    </div>
   );
 }
